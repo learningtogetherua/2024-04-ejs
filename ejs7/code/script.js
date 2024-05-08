ae84947dcf2dd3c3e7e7dbee386925f1463cc8ab
learningtogetherua <83807055+learningtogetherua@users.noreply.github.com>
2024-05-08 10:57:17 +0300

    Add files via upload

---
+++ ejs7/code/script.js
@@ -0,0 +1,145 @@
+// елементарні типи незмінними (immutable)
+// let mess = "Privyt"
+// console.log(mess);
+// // mess = 'bye!'
+// mess = 'Priyt'
+// console.log(mess);
+
+// let digit = 6
+// digit = true
+
+
+// об'єктні структури змінні (mutable)
+// let testArray = []
+// testArray[0] = false
+
+// let testObj = {}
+// testObj.dateOfBirthday = 12
+// console.log(testObj);
+
+// const testArray = []
+// // testArray[0] = false
+// console.log(testArray);
+
+// const testObj = {}
+// testObj.dateOfBirthday = 12
+// console.log(testObj);
+
+// Explicit Type Conversion
+// String(true)
+// console.log(typeof String(true))
+
+// Implicit Type Conversion
+// let x = 5
+// let y = '10'
+
+// let result = x + y
+// console.log(typeof result);
+// console.log(result);
+
+// let result = y + x
+// console.log(typeof result);
+// console.log(result);
+
+// let resultUnar = + y
+// let resultUnar = + y + x
+// console.log(resultUnar);
+// console.log(typeof resultUnar);
+
+// let x = 5
+// let y = true
+// let result = x + y
+// console.log(typeof result);
+// console.log(result);
+
+// let x = 5
+// let y = '10'
+// // let y = '10a'
+
+// // let result = x - y
+// let result = - y
+// console.log(typeof result);
+// console.log(result);
+
+// let x = 5
+// let y = '10'
+
+// let result = x * y
+// console.log(typeof result);
+// console.log(result);
+
+// неймінг
+// lowerCamelCase
+
+// let resultUnar // lowerCamelCase
+// let result_unar_else // snake_case
+
+// // const MAX_LENGTH = 100
+// // const PI_VALUE = 3.1415
+// const MAXLENGTH = 100
+// const PIVALUE = 3.1415
+
+
+//  унарні оператори (операція з одним операндом)
+// -x // зміна значення на протилежне (додатне->від'ємне або навпаки)
+// +x // спроба конвертувати значення в числове
+// ++y // інкремент (збільшення на 1)
+// --y // декремент (зменшення на 1)
+
+// бінарні оператори (операція з двома операторами)
+// x + y
+// x - y
+// x * y
+// x / y
+// x % y
+// x ** y
+
+// testStr.toLowercase()
+// let x=0, y=10, z=-2
+// for (let i = 0, k = 2, j = 23; i < 10; i++) {
+
+
+// }
+
+// тернарний оператор
+// (умова) ? (дія1) : (дія2)
+
+
+// варіанти оголошення змінних
+// let message = "hello";
+// let i = 0, j = 0, k = 0;
+// let i = y = x = j = 0
+// console.log(i, y, x, j);
+// let x = 2, y = x * x;
+// console.log(x, y);
+
+//  альтернативні оператори
+
+// let digit1 = 45
+// // digit1 = digit1 + 5
+// // digit1 += 5 // 50
+// // digit1 -= 5 // 40
+// // digit1 *= 5 // 225
+// // digit1 /= 5 // 9
+// // digit1 %= 5 // 0
+// // digit1 %= 4 // 1
+// // digit1 **= 2 // 2025
+// console.log(digit1);
+
+// let test = 10
+// ++test
+// --test
+// test++ 
+// test--
+
+// console.log(++test); // 11
+// console.log(test+1); // 11
+// test+=1
+// console.log(test); // 11
+// console.log(--test); // 9
+
+// console.log(test++); // 10
+// console.log(test); // 11
+// console.log(test--); // 10
+// console.log(test); // 9
+
---
+++ ejs7/info/ejs7-links.md
@@ -0,0 +1,10 @@
+# Посилання - Урок 7
+
+
+- [Змінність (Mutable)](https://developer.mozilla.org/en-US/docs/Glossary/Mutable)
+- [Незмінність (Immutable)](https://developer.mozilla.org/en-US/docs/Glossary/Immutable)
+- [Поради по іменуванню змінних](https://sentry.io/answers/naming-variables-in-javascript/)
+- [Camel case](https://developer.mozilla.org/en-US/docs/Glossary/Camel_case)
+- [Snake case](https://developer.mozilla.org/en-US/docs/Glossary/Snake_case)
+- [Kebab case](https://developer.mozilla.org/en-US/docs/Glossary/Kebab_case)
+- [ВСІ оператори в JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_operators)
