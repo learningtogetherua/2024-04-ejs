aa289aa5cc994ef419994f0346398a14a2052e4c
learningtogetherua <83807055+learningtogetherua@users.noreply.github.com>
2024-05-15 00:44:52 +0300

    Add files via upload

---
+++ ejs9/code/script.js
@@ -0,0 +1,119 @@
+// AND &&
+// умова1 && умова2 && умова3 && умова4 
+
+// let age = 25;
+// // const hasDriverLicense = true;
+// const hasDriverLicense = false;
+
+// if (age >= 18 && hasDriverLicense) {
+//   console.log("Ви можете отримати водійське посвідчення.");
+// } else {
+//   console.log("Ви не можете отримати водійське посвідчення.");
+// }
+
+// let x = 5
+// // let x = 0
+// // let y = "Hello"
+// let y = null
+// // let arr = [1,2]
+// let arr = undefined
+
+// let result = x && y && arr
+// console.log(result);
+
+// OR ||
+// умова1 || умова2 || умова3 || умова4 
+
+// let isStudent = false;
+// let hasJob = true;
+
+// if (isStudent || hasJob) {
+//     console.log("Ви можете отримати кредит на навчання або роботу.");
+// } else {
+//     console.log("Ви не можете отримати кредит на навчання або роботу.");
+// }
+
+// let x = null
+// // let x = 0
+// // let y = 0
+// let y = 100
+// // let arr = undefined
+// let arr = [1,2]
+
+// let result = x || y || arr
+// console.log(result);
+
+// NOT !
+
+// console.log(!200); // !Boolean(200) 
+// console.log(!!200); // Boolean(200) 
+
+// // let haveMoney = true
+// let haveMoney = false
+
+// if (!haveMoney) {
+//     console.log('bad');
+// } else {
+//     console.log('good');
+// }
+
+// loops
+
+// while (умова) {
+//     // код, що виконується, доки умова є істинною
+//   }
+
+// let i = 0; // i j k count counter
+// let i = 1;
+// let i = 6;
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+//   }
+// console.log('дії після завершення циклу');
+
+// do {
+//     // код, що виконується
+//   } while (умова); // тут крапка з комою обов'язкова
+
+// let i = 0;
+// let i = 4;
+// do {
+//     console.log(i);
+//         i++;
+// } while (i < 5);
+
+// for (ініціалізація; умова; інкремент) {
+//     // код, що виконується на кожному кроці циклу
+//   }
+
+// for (let i = 0; i < 5; i++) {
+
+//     console.log(i);
+
+// }
+
+// for (let i = 0, j=10; i < 5; i++, j--) {
+
+//     console.log('i '+i);
+//     console.log('j '+j);
+
+// }
+
+// let i = 0
+// for (; i < 5; i++) {
+
+//     console.log(i);
+
+// }
+
+// let i = 0
+// for ( ; i < 5; ) {
+
+//     console.log(i);
+//     i++
+// }
+
+for(;;) {
+    console.log('infinityyyyyy');
+}
---
+++ ejs9/info/ejs9-links.md
@@ -0,0 +1,16 @@
+# Посилання - Урок 9
+
+## Оператори логіки
+- [AND - &&](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_AND)
+- [OR - ||](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_OR)
+- [NOT - !](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_NOT)
+- [Оператор нульового об’єднання - ??](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing)
+
+## Пріоритетність всіх операторів в JavaScript
+- [Understanding Operator Precedence in JavaScript](https://www.almabetter.com/bytes/tutorials/javascript/precedence-of-operators)
+- [MDN - Operator precedence](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_precedence)
+
+##  Цикли
+- [While](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while)
+- [Do While](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/do...while)
+- [For](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for)
